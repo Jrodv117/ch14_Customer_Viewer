9a93093331e58bd5c9c61fa0dc7dec2d8d032072
jrodr117 <jrodr113@msudenver.edu>
2022-10-26 23:34:14 -0600

    added read_csv function with a try block to return customer list if found. and to raise exception if no list is no csv is found

--- Customer.py
+++ Customer.py
@@ -1,22 +1,28 @@
 
+from audioop import add
+from os import stat
+
+
 class Customer:
-    def __init__(self):
-        self.id
-        self.firstName
-        self.lastName
-        self.company
-        self.address
-        self.city
-        self.state
-        self.zip
+    def __init__(self, id, firstName, lastName, company, address, city, state, zip):
+        self.cust_id=id
+        self.first_name=firstName
+        self.last_name=lastName
+        self.company=company
+        self.address=address
+        self.city=city
+        self.state=state
+        self.zip=zip
+        
     
-    def getfull_address(self):
+    
+    def __str__(self):
         full_address=''
         
         if self.company=="":
-            full_address+=self.firstName+" "+self.lastName+"\n"+self.address+"\n"+self.city+", " +self.state+" "+str(self.zip)
+            full_address+=self.first_name+" "+self.last_name+"\n"+self.address+"\n"+self.city+", " +self.state+" "+str(self.zip)
         else:
-            full_address+=self.firstName+" "+self.lastName+"\n"+self.company+"\n"+self.address+"\n"+self.city+", " +self.state+" "+str(self.zip)
+            full_address+=self.first_name+" "+self.last_name+"\n"+self.company+"\n"+self.address+"\n"+self.city+", " +self.state+" "+str(self.zip)
         return full_address
     
     
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from Customer import Customer
+import csv
 
-customer = Customer(101,"James","Butler","","6649 N Blue Gum St","New Orleans","LA",70116)
 
-ya = customer.getfull_address()
-
-print(ya)
+def read_csv():
+    try:
+        with open('customers.csv') as data_file:
+            f = csv.reader(data_file)
+            customer_list=[]
+            for row in f:
+                customer_list.append(Customer(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],))
+            return customer_list
+    except:
+        print("ERROR CSV FILE NOT FOUND OR COULDN'T BE READ")
+        exit()
